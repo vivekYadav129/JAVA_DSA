46bc1003263c1d8673060a2fa61c5365a1bce24d
Vivek Yadav <v8004282@gmail.com>
2026-08-10 13:59:30 +0530

    Add Question5

---
+++ Practice_Questions/Question5.java
@@ -0,0 +1,13 @@
+class Question5{
+    public static void main(String[] args){
+        int a = 12;
+        int b = 40;
+
+        if(b>a){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(a);
+        }
+    }
+}

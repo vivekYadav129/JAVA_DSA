38ba2af81e3acfbd5043826491d5e0fb81141cf1
Vivek Yadav <v8004282@gmail.com>
2026-08-11 18:44:35 +0530

    commit message

---
+++ Nested_Loop/Question3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Question3{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int age = sc.nextInt();
+    if(age > 18){
+        System.out.println("Eligible for Voting");
+    } 
+    else {
+        System.out.println("Not Eligible");
+    }   
+
+}
+
+
+}

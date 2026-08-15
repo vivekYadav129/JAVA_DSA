eae85cc758ce4496820af9478d7288f94a38d451
Vivek Yadav <v8004282@gmail.com>
2026-08-15 17:15:42 +0530

    print sum of first n natural number

---
+++ Patterns/Question5.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Question5{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = n*(n+1)/2;
+        System.out.println(sum);
+    }
+}

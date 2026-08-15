1ad583e6cad7aaef39a2d97b5bd5aa0bed7e5cb2
Vivek Yadav <v8004282@gmail.com>
2026-08-15 17:40:02 +0530

    Factorial of a given numbers

---
+++ Patterns/Question7.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Question7 {
+    static int fact(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int result = fact(n);
+
+        System.out.println(result);
+    }
+}

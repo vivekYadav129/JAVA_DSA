b67aa3f06014b4bc119f82ef4a08dee7e2fb75a5
Vivek Yadav <v8004282@gmail.com>
2026-08-16 19:51:45 +0530

    Count he number

---
+++ Looping_Logic/Question1.java
@@ -0,0 +1,13 @@
+class Question1 {
+    public static void main(String[] args) {
+        int n = 12345;
+        int count = 0;
+
+        while (n != 0) {
+            n = n / 10;
+            count++;
+        }
+
+        System.out.println("Number of digits = " + count);
+    }
+}

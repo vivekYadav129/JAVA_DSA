f843c841f514d3788b4d4ef4d4d07f606d8beefb
Vivek Yadav <v8004282@gmail.com>
2026-08-17 03:18:59 +0530

    printcubes

---
+++ Logical_patterns/Question2.java
@@ -0,0 +1,10 @@
+class Question2{
+    static void printcubes(int n){
+        for(int i=0; i<=n; i++){
+            System.out.println(i*i*i);
+        }
+    }
+    public static void main(String[] args){
+        printcubes(6);
+    }
+}

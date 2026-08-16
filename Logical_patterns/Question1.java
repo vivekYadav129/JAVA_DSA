326977b96313ed2a87583c7871249e98a8b2eab1
Vivek Yadav <v8004282@gmail.com>
2026-08-17 02:59:26 +0530

    squares of a number

---
+++ Logical_patterns/Question1.java
@@ -0,0 +1,12 @@
+class Question1 {
+
+    static void printSquares(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i * i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printSquares(5);
+    }
+}

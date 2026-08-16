15d53ba49e4cbcf8b6b2bbdfea94ba0e9f69711f
Vivek Yadav <v8004282@gmail.com>
2026-08-16 19:56:14 +0530

    sum of digit

---
+++ Looping_Logic/Question4.java
@@ -0,0 +1,14 @@
+class Question4 {
+    public static void main(String[] args) {
+        int n = 12345;
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            sum = sum + digit;
+            n = n / 10;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}

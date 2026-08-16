af054cfb4406f7fe2f038f05c26d61234844690b
Vivek Yadav <v8004282@gmail.com>
2026-08-16 19:59:25 +0530

    perfect number

---
+++ Looping_Logic/Question6.java
@@ -0,0 +1,18 @@
+class Question6 {
+    public static void main(String[] args) {
+        int n = 6;
+        int sum = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == n) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not a Perfect Number");
+        }
+    }
+}

9fa498728c003684d4b497c69ccd4be5c422ec96
Vivek Yadav <v8004282@gmail.com>
2026-09-01 18:18:51 +0530

    print numbers 1 and 0

---
+++ Patterns_Problem/Pattern12.java
@@ -0,0 +1,13 @@
+public class Pattern12{
+    public static void main(String[] args){
+        int rows = 5;
+        
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=i; j++){
+                int val = (i+j)%2;
+                System.out.print(val + " "); 
+            }
+            System.out.println();
+        } 
+    }
+}

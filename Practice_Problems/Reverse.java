0a81b22aa9ad39ebf6e88e1a5c7be44114a762fd
Vivek Yadav <v8004282@gmail.com>
2026-09-13 00:05:36 +0530

    Print reverse of array

---
+++ Practice_Problems/Reverse.java
@@ -0,0 +1,22 @@
+class Reverse{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int left = 0;
+        int right = arr.length - 1;
+       while(left < right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+
+       }
+       for(int i=0; i<arr.length; i++){
+        System.out.print(arr[i] + " ");
+       }
+      
+
+            
+        }
+    }
